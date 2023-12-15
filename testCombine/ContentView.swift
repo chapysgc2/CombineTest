21e4efbbd84ab63519cb4b0efc3d45e8340b6cb3
Hazel Alain <hazelo235@gmail.com>
2023-12-15 16:44:34 -0600

    Initial Commit

---
+++ testCombine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  testCombine
+//
+//  Created by Hazel Alain on 15/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testCombine/testCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  testCombineApp.swift
+//  testCombine
+//
+//  Created by Hazel Alain on 15/12/23.
+//
+
+import SwiftUI
+
+@main
+struct testCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
